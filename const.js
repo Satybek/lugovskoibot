78d2f7ec54eb53bf45e1032a0f9746cda0791df2
Satybek <satybek92@mail.ru>
2022-07-05 02:45:41 +0600

    first commit

---
+++ const.js
@@ -0,0 +1,14 @@
+const commands = `
+/start - Перезапустить бота
+/help - Помощь
+`;
+
+const text1 = `
+<a> href="https://www.youtube.com/"</a>`;
+
+const text2 = `
+<a> href="https://www.youtube.com/"</a>`;
+
+module.exports.commands = commands;
+module.exports.text1 = text1;
+module.exports.text2 = text2;
